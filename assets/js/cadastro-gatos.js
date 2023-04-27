9b9054850701534f6ef001ca290cfb03ad6b56fd
Gabriela Pereira da Costa <gabriela-pc@hotmail.com>
2023-04-27 16:46:26 -0300

    modificação no css do cadastro e do salvemento de img localstorage e botão no JS

--- assets/js/cadastro-gatos.js
+++ assets/js/cadastro-gatos.js
@@ -4,25 +4,47 @@ function salvarForm (event) {
     event.preventDefault();
     const catname = document.querySelector('#m-name').value;
     const hname = document.querySelector('#h-name').value;
+    const formImg = document.querySelector('#upload-file').files[0];
+    const catPreview = document.querySelector('#cat-svg');
 
     const formdata = {
-        catname, hname 
+        catname, 
+        hname, 
+        formImg: formImg?.name || 'assets/gato-1.jpeg'
     }
     
-    const dadossalvos = localStorage.getItem('Form');
-    let todososdados = []
+    const savedData = localStorage.getItem('Form');
+    let allData = []
     
-    if (dadossalvos) {
-        todososdados = JSON.parse(dadossalvos);
+    if (savedData) {
+        allData = JSON.parse(savedData);
 
     }
-    todososdados.push(formdata); 
+    allData.push(formdata); 
 
-    localStorage.setItem('Form', JSON.stringify(todososdados));
+    localStorage.setItem('Form', JSON.stringify(allData));
 
+    catPreview.src = 'assets/img/cat-svg.png';
     form.reset();
+    
 
 }
+
+form.addEventListener("submit", salvarForm);
+
+const uploadFile = document.querySelector('#upload-file');
+const catPreview = document.querySelector('#cat-svg');
+uploadFile.addEventListener('change', (event) => {
+  const file = uploadFile.files[0];  
+  if (file) {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+        catPreview.src = reader.result;
+    };
+  }
+
+});
 
 let inputCatName = document.querySelector('#m-name')
 let inputOwnerName = document.querySelector('#h-name')
@@ -38,3 +60,6 @@ function verify () {
 
 inputCatName.addEventListener('input', verify);
 inputOwnerName.addEventListener('input', verify);
+
+
+
